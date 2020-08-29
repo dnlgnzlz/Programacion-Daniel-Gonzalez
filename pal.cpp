88052ba7f268083afadda60d2be5c415edbccbb7
daniel gonzalez <dnlgonzalezh@gmail.com>
2020-08-29 11:55:57 -0500

    palidrome

---
+++ pal.cpp
@@ -0,0 +1,35 @@
+
+// actividad de clase #1
+// deteccion de palabras palindromes 
+// alumno: Nicolas Jimnez
+
+
+#include <iostream>
+#include <cstdlib>
+ 
+using namespace std;
+ 
+int main() {
+ 
+	string palabra;
+ 
+	cout<<"Ingrese palabra:"<<endl;
+	cin >> palabra;
+ 
+	size_t longitud = palabra.length()-1;
+	size_t i=0;
+	bool esPalindromo = true;
+ 
+	for(i=0 ; i<palabra.length()/2 && esPalindromo ;i++) {
+ 
+		if(palabra[i]!=palabra[longitud-i]){
+			esPalindromo = false;
+		}
+ 
+	}
+ 
+    cout<<((true == esPalindromo) ? "es palíndromo" : "no es palíndromo")<<endl;
+ 
+	return EXIT_SUCCESS;
+}
+ 
